22b7395fef497b68b682bbb1c65beec51df08c35
Joseph Nasr <Joseph.Nasr@gotocme.com>
2018-09-18 02:07:09 +0300

    Created the "Send Nudes" animation.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,38 @@
 
-window.onload = function() {
-    var width = Math.max(960, innerWidth),
-        height = Math.max(500, innerHeight);
-    
-    var i = 0;
-    
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-    
+window.onload = function () {
+    let width = Math.max(960, innerWidth);
+    let height = Math.max(500, innerHeight);
+
+    let i = 0;
+
+    let svg = d3.select("body")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height);
+
     svg.append("rect")
         .attr("width", width)
         .attr("height", height)
         .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
-    
+
     function particle() {
-        var m = d3.mouse(this);
-    
-        svg.insert("circle", "rect")
-            .attr("cx", m[0])
-            .attr("cy", m[1])
-            .attr("r", 1e-6)
-            .style("stroke", d3.hsl((i = (i + 1) % 360), 1, .5))
+        let m = d3.mouse(this);
+
+        svg.insert("text", "rect")
+            .attr("x", m[0])
+            .attr("y", m[1])
+            .html("SEND NUDES")
+            .style("font-family", "'Josefin Sans'")
+            .style("font-size", 1e-6)
+            .style("fill", d3.hsl((i = (i + 1) % 360), 1, .5))
             .style("stroke-opacity", 1)
             .transition()
             .duration(2000)
             .ease(Math.sqrt)
-            .attr("r", 100)
-            .style("stroke-opacity", 1e-6)
+            .style("font-size", 30)
+            .style("fill-opacity", 1e-6)
             .remove();
-    
+
         d3.event.preventDefault();
     }
 }
