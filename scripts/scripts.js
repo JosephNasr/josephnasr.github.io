73b16c49ff742c1cefd1409a41f5d4d7f6de7684
Joseph Nasr <Joseph.Nasr@gotocme.com>
2018-09-22 22:43:07 +0300

    Added the hue transition animation.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,36 +1,38 @@
-
 window.onload = function () {
     let width = Math.max(960, innerWidth);
     let height = Math.max(500, innerHeight);
 
-    let i = 0;
-
     let svg = d3.select("body")
                 .append("svg")
                 .attr("width", width)
-                .attr("height", height);
+                .attr("height", height)
+                .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
 
-    svg.append("rect")
-        .attr("width", width)
-        .attr("height", height)
-        .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
+    let hue = 0;
+    let saturation = 0;
+    let direction = 1;
 
     function particle() {
-        let m = d3.mouse(this);
+        let mousePosition = d3.mouse(this);
 
-        svg.insert("text", "rect")
-            .html("SEND NUDES")
-            .attr("x", m[0])
-            .attr("y", m[1])
+        svg.insert("text", "svg")
+            .html("Joseph Nasr")
+            .attr("x", mousePosition[0])
+            .attr("y", mousePosition[1])
+            .style("user-select", "none")
             .style("font-family", "'Josefin Sans'")
             .style("font-size", 1e-6)
-            .style("fill", d3.hsl((i = (i + 1) % 360), 1, .5))
+            .style("fill", d3.hsl(hue, saturation / 100, .5))
             .transition()
             .duration(2000)
             .ease(Math.sqrt)
             .style("font-size", 30)
             .style("fill-opacity", 1e-6)
             .remove();
+
+        hue = ++hue % 360;
+        saturation += direction;
+        direction *= saturation % 100 == 0 ? -1 : 1;
 
         d3.event.preventDefault();
     }
