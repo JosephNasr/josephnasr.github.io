002ed05c4d356e82cdc1da895b637395a95215ce
Joseph Nasr <Joseph.Nasr@gotocme.com>
2018-11-19 16:44:53 +0200

    Reverted back to the circle animation.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,38 +1,35 @@
 window.onload = function () {
-    let width = Math.max(960, innerWidth);
-    let height = Math.max(500, innerHeight);
+    const width = Math.max(960, innerWidth);
+    const height = Math.max(500, innerHeight);
 
-    let svg = d3.select("body")
-        .append("svg")
+    let counter = 0;
+
+    const svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.append("rect")
         .attr("width", width)
         .attr("height", height)
         .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
 
-    let hue = 0;
-    let saturation = 0;
-    let direction = 1;
-
     function particle() {
-        let mousePosition = d3.mouse(this);
+        const mousePosition = d3.mouse(this);
 
-        svg.insert("text", "svg")
-            .html("SEND NUDES")
-            .attr("x", mousePosition[0])
-            .attr("y", mousePosition[1])
-            .style("user-select", "none")
-            .style("font-family", "'Josefin Sans'")
-            .style("font-size", 1e-6)
-            .style("fill", d3.hsl(hue, saturation / 100, .5))
+        svg.insert("circle", "rect")
+            .attr("cx", mousePosition[0])
+            .attr("cy", mousePosition[1])
+            .attr("r", 1e-6)
+            .style("stroke", d3.hsl(counter, 1, .5))
+            .style("stroke-opacity", 1)
             .transition()
             .duration(2000)
             .ease(Math.sqrt)
-            .style("font-size", 30)
-            .style("fill-opacity", 1e-6)
+            .attr("r", 100)
+            .style("stroke-opacity", 1e-6)
             .remove();
 
-        hue = ++hue % 360;
-        saturation += direction;
-        direction *= saturation % 100 == 0 ? -1 : 1;
+        counter = (counter + 1) % 360;
 
         d3.event.preventDefault();
     }
