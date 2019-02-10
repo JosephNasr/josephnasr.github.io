e5e6834b5fc3b5f0fcd4d51fa122135ecee6d838
Joseph Nasr <J.nsr@hotmail.com>
2019-02-10 22:54:38 +0200

    Reverted to the Send Nudes animation.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,38 @@
 window.onload = function () {
-    const width = Math.max(960, innerWidth);
-    const height = Math.max(500, innerHeight);
+    let width = Math.max(960, innerWidth);
+    let height = Math.max(500, innerHeight);
 
-    let counter = 0;
-
-    const svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-    svg.append("rect")
+    let svg = d3.select("body")
+        .append("svg")
         .attr("width", width)
         .attr("height", height)
         .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
 
-    function particle() {
-        const mousePosition = d3.mouse(this);
+    let hue = 0;
+    let saturation = 0;
+    let direction = 1;
 
-        svg.insert("circle", "rect")
-            .attr("cx", mousePosition[0])
-            .attr("cy", mousePosition[1])
-            .attr("r", 1e-6)
-            .style("stroke", d3.hsl(counter, 1, .5))
-            .style("stroke-opacity", 1)
+    function particle() {
+        let mousePosition = d3.mouse(this);
+
+        svg.insert("text", "svg")
+            .html("SEND NUDES")
+            .attr("x", mousePosition[0])
+            .attr("y", mousePosition[1])
+            .style("user-select", "none")
+            .style("font-family", "'Josefin Sans'")
+            .style("font-size", 1e-6)
+            .style("fill", d3.hsl(hue, saturation / 100, .5))
             .transition()
             .duration(2000)
             .ease(Math.sqrt)
-            .attr("r", 100)
-            .style("stroke-opacity", 1e-6)
+            .style("font-size", 30)
+            .style("fill-opacity", 1e-6)
             .remove();
 
-        counter = (counter + 1) % 360;
+        hue = ++hue % 360;
+        saturation += direction;
+        direction *= saturation % 100 == 0 ? -1 : 1;
 
         d3.event.preventDefault();
     }
