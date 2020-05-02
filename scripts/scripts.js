dff7e3c344dcdd7347cccdcbeba79131cad0a509
Joseph.Nasr <Joseph.Nasr@gotocme.com>
2020-05-02 15:24:52 +0300

    Updated to LinkedIn badge.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,36 +1,47 @@
 window.onload = function () {
-    const width = Math.max(960, innerWidth);
-    const height = Math.max(500, innerHeight);
+    // const width = Math.max(960, innerWidth);
+    // const height = Math.max(500, innerHeight);
 
     let counter = 0;
 
-    const svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+    var colors = ["white", "red", "orange", "yellow", "green", "blue", "purple", "blue", "green", "yellow", "orange"];
+    var currentIndex = 0;
 
-    svg.append("rect")
-        .attr("width", width)
-        .attr("height", height)
-        .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
+    setInterval(function () {
+        document.body.style.cssText = "background-color: " + colors[currentIndex];
+        currentIndex++;
+        if (currentIndex == undefined || currentIndex >= colors.length) {
+            currentIndex = 0;
+        }
+    }, 1000);
 
-    function particle() {
-        const mousePosition = d3.mouse(this);
+    // const svg = d3.select("body").append("svg")
+    //     .attr("width", width)
+    //     .attr("height", height);
 
-        svg.insert("circle", "rect")
-            .attr("cx", mousePosition[0])
-            .attr("cy", mousePosition[1])
-            .attr("r", 1e-6)
-            .style("stroke", d3.hsl(counter, 1, .5))
-            .style("stroke-opacity", 1)
-            .transition()
-            .duration(2000)
-            .ease(Math.sqrt)
-            .attr("r", 100)
-            .style("stroke-opacity", 1e-6)
-            .remove();
+    // svg.append("rect")
+    //     .attr("width", width)
+    //     .attr("height", height)
+    //     .on("ontouchstart" in document ? "touchmove" : "mousemove", particle);
 
-        counter = (counter + 1) % 360;
+    // function particle() {
+    //     const mousePosition = d3.mouse(this);
 
-        d3.event.preventDefault();
-    }
+    //     svg.insert("circle", "rect")
+    //         .attr("cx", mousePosition[0])
+    //         .attr("cy", mousePosition[1])
+    //         .attr("r", 1e-6)
+    //         .style("stroke", d3.hsl(counter, 1, .5))
+    //         .style("stroke-opacity", 1)
+    //         .transition()
+    //         .duration(2000)
+    //         .ease(Math.sqrt)
+    //         .attr("r", 100)
+    //         .style("stroke-opacity", 1e-6)
+    //         .remove();
+
+    //     counter = (counter + 1) % 360;
+
+    //     d3.event.preventDefault();
+    // }
 }
